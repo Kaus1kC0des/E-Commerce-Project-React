2ffb691189bb99323a8e51c0d8065173f0bf81ce
Kausik D <kausikdevanathan@gmail.com>
2025-12-13 23:54:45 +0530

    Removed starter-code and included tracking Component, seperated Header into a single individual component, and added Link to disable reload on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {HomePage} from "./pages/HomePage";
 import {CheckoutPage} from "./pages/CheckoutPage.jsx";
 import {OrdersPage} from "./pages/OrdersPage.jsx";
+import {TrackingPage} from "./pages/TrackingPage.jsx";
 import "./App.css";
 import {Route, Routes} from "react-router";
 
@@ -10,6 +11,7 @@ function App() {
             <Route index element={<HomePage/>}/>
             <Route path="/checkout" element={<CheckoutPage/>}/>
             <Route path="/orders" element={<OrdersPage/>}/>
+            <Route path="/tracking" element={<TrackingPage/>}/>
         </Routes>
     );
 }
