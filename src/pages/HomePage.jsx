0121a1baa15f429e678903be153996db486ae371
Kausik D <kausikdevanathan@gmail.com>
2025-12-13 23:00:41 +0530

    Added titles to routes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import "./HomePage.css";
 export default function HomePage() {
     return (
         <>
+            <title>Home</title>
             <div className="header">
                 <div className="left-section">
                     <a href="index.html" className="header-link">
