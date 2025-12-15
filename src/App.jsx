c50d4b95d579aa4e99bfe9ec9e46f4144808e4e6
Kausik D <kausikdevanathan@gmail.com>
2025-12-15 22:47:48 +0530

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {HomePage} from "./pages/HomePage";
 import {CheckoutPage} from "./pages/checkout/CheckoutPage.jsx";
 import {OrdersPage} from "./pages/OrdersPage.jsx";
 import {TrackingPage} from "./pages/TrackingPage.jsx";
+import {NotFoundPage} from "./pages/404.jsx";
 import "./App.css";
 import {Route, Routes} from "react-router";
 
@@ -12,6 +13,7 @@ function App() {
             <Route path="/checkout" element={<CheckoutPage/>}/>
             <Route path="/orders" element={<OrdersPage/>}/>
             <Route path="/tracking" element={<TrackingPage/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
     );
 }
---
+++ src/pages/404.jsx
@@ -0,0 +1,13 @@
+import {Header} from "../components/Header.jsx";
+
+export function NotFoundPage() {
+    return (
+        <>
+            <title>404 - Not Found</title>
+            <Header/>
+            <h1 style={{marginTop: "45vh", marginLeft: "45vw"}}>
+                404 - Page Not Found
+            </h1>
+        </>
+    )
+}
