e5446606957949cee9de8c21bb010e7142ec0d92
Kausik D <kausikdevanathan@gmail.com>
2026-01-04 13:56:13 +0530

    Added key prop to Product component

--- ecommerce-frontend/src/pages/home/ProductsGrid.jsx
+++ ecommerce-frontend/src/pages/home/ProductsGrid.jsx
@@ -5,7 +5,7 @@ export function ProductsGrid({products, loadCart}) {
         <div className="products-grid">
             {
                 products.map((product) =>{
-                    return <Product product={product} loadCart={loadCart}/>
+                    return <Product product={product} loadCart={loadCart} key={product.id}/>
                 })
             }
         </div>
