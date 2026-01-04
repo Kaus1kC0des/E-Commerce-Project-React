e6d54b65b1a9f48331ca4110f0f982a65ca3a5a6
Kausik D <kausikdevanathan@gmail.com>
2026-01-04 21:37:40 +0530

    Enabled the Update button in CartItemDetails page

--- ecommerce-frontend/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-frontend/src/pages/checkout/CartItemDetails.jsx
@@ -2,15 +2,25 @@ import dayjs from "dayjs";
 import {formatMoney} from "../../utils/money.js";
 import {DeliveryOptions} from "./DeliveryOptions.jsx";
 import axios from "axios";
+import {useState} from "react";
 
 export function CartItemDetails({cart, deliveryOptions, loadCart}) {
+    const [editQty, setEditQty] = useState(false);
+    const [qty, setQty] = useState(0);
     return (cart.map((cartItem) => {
             let selectedDeliveryOption = deliveryOptions.find((option) => option.id === cartItem.deliveryOptionId);
             const deleteCartItem = async () => {
                 await axios.delete(`/api/cart-items/${cartItem.productId}`);
                 await loadCart();
             }
-            return (<div className="cart-item-container" key={cartItem.productId}>
+            const updateItem = async () => {
+                await axios.put(`/api/cart-items/${cartItem.productId}`, {
+                    quantity: qty,
+                });
+                await loadCart();
+            }
+            return (
+                <div className="cart-item-container" key={cartItem.productId}>
                     <div className="delivery-date">
                         Delivery
                         date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format("dddd, DD MMMM YYYY")}
@@ -31,14 +41,54 @@ export function CartItemDetails({cart, deliveryOptions, loadCart}) {
                             <div className="product-quantity">
                                                     <span>
                                                       Quantity:
-                                                        <span
-                                                            className="quantity-label">{cartItem.quantity}
-                                                        </span>
+
+                                                        {
+                                                            editQty ?
+                                                                <>
+                                                                    <input
+                                                                        type="text"
+                                                                        placeholder={cartItem.quantity}
+                                                                        style={{width: "50px"}}
+                                                                        className="quantity-editor"
+                                                                        onChange={(event) => setQty(Number(event.target.value))}
+                                                                    />
+                                                                    <span
+                                                                        className="update-quantity-link link-primary"
+                                                                        onClick={async () => {
+                                                                            await updateItem();
+                                                                            setEditQty(!editQty);
+                                                                        }}
+                                                                    >
+                                                                        Save
+                                                                    </span>
+                                                                    <span
+                                                                        className="update-quantity-link link-primary"
+                                                                        onClick={() => {
+                                                                            setQty(0);
+                                                                            setEditQty(!editQty);
+                                                                        }}
+                                                                    >
+                                                                        Cancel
+                                                                    </span>
+                                                                </>
+                                                                :
+                                                                <span
+                                                                    className="quantity-label">{cartItem.quantity}
+                                                                </span>
+                                                        }
+
                                                     </span>
                                 <br/>
-                                <span className="update-quantity-link link-primary">
+                                <button
+                                    type="button"
+                                    className="update-quantity-link link-primary button-link"
+                                    onClick={async () => {
+                                        setEditQty(!editQty);
+                                    }}
+                                    disabled={editQty}
+                                >
                                   Update
-                                </span>
+                                </button>
                                 <span
                                     className="delete-quantity-link link-primary"
                                     onClick={deleteCartItem}
